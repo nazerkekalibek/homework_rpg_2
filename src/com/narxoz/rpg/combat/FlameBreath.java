64aa5f275f14f33f2a0293001365757f8e39983d
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-02-20 03:41:35 +0500

    Added FlameBreath implements

--- src/com/narxoz/rpg/combat/FlameBreath.java
+++ src/com/narxoz/rpg/combat/FlameBreath.java
@@ -1,12 +1,19 @@
 package com.narxoz.rpg.combat;
 
 public class FlameBreath implements Ability {
+    @Override
     public String getName(){
         return "flame breath";
     }
+    @Override
     public int getDamage(){
         return 250;
     }
+    @Override
+    public String getDescription(){
+        return "Deals area fire damage and burns enemies ";
+    }
+    @Override
     public Ability clone(){
         return new FlameBreath();
     }
