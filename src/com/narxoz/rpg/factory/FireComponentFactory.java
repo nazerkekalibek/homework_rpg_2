ae462133ae6901a20ae5929c42b65c6e2bb5e34f
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-02-21 02:17:08 +0500

    Added FireComponentFactory

---
+++ src/com/narxoz/rpg/factory/FireComponentFactory.java
@@ -0,0 +1,24 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.*;
+import com.narxoz.rpg.loot.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FireComponentFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities(){
+        List<Ability> abilities=new ArrayList<>();
+        abilities.add(new FlameBreath());
+        abilities.add(new FireShield());
+        return abilities;
+    }
+    @Override
+    public LootTable createLootTable() {
+        return new FireLootTable();
+    }
+    @Override
+    public String createAIBehavior() {
+        return "aggressive fire AI";
+    }
+}
