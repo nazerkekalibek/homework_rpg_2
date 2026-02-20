70774b6b6367a654312e17e2d2a2a68fe5386392
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-02-20 21:26:03 +0500

    Added Enemy interface

--- src/com/narxoz/rpg/enemy/Enemy.java
+++ src/com/narxoz/rpg/enemy/Enemy.java
@@ -2,7 +2,6 @@ package com.narxoz.rpg.enemy;
 
 import com.narxoz.rpg.combat.Ability;
 import com.narxoz.rpg.loot.LootTable;
-
 import java.util.List;
 
 
@@ -12,5 +11,5 @@ public interface Enemy {
     int getDamage();
     List<Ability> getAbilities();
     LootTable getLootTable();
-    Enemy clone();
+    Enemy copy();
 }
